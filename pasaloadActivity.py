7b52f7d389d2e702ac03d031d8184e79b5bb5b2d
DoubleWoot <202110423@feualabang.edu.ph>
2023-05-12 15:07:40 +0800

    Initial Commit

---
+++ pasaloadActivity.py
@@ -0,0 +1,77 @@
+import threading
+import time
+import random
+
+load_balance = 0
+x = True
+
+if load_balance == 0:
+    random_load = random.randint(1, 50)
+    load_balance = random_load
+
+def transaction(amount):
+    global load_balance
+    load_balance += amount
+    print("\nYour balance is now: ", load_balance)
+
+
+def add_load():
+    global x
+    global y
+
+    print("\nHow much do you want to add?")
+    print("""
+    1. P20
+    2. P50
+    3. P100""")
+
+    while x == True:
+        choice = input("Select amount or press 'e' to exit: ")
+        if (choice == '1'):
+            thread1 = threading.Thread(target=transaction, args=(20,))
+            y = threading.Thread(target=add_load)
+            y.start()
+            thread1.start()
+            thread1.join()
+        elif (choice == '2'):
+            thread2 = threading.Thread(target=transaction, args=(50,))
+            y = threading.Thread(target=add_load)
+            y.start()
+            thread2.start()
+            thread2.join()
+        elif (choice == '3'):
+            thread3 = threading.Thread(target=transaction, args=(100,))
+            y = threading.Thread(target=add_load)
+            y.start()
+            thread3.start()
+            thread3.join()
+        elif (choice == 'e'):
+            y.join()
+            x = False
+
+
+
+def main():
+    global load_balance
+    print("\n----Pasaload Service----")
+    phoneNum = input("Enter your phone number: ")
+    print("Checking your load balance...\n")
+    time.sleep(3)
+    print(phoneNum, "has a balance of:", load_balance)
+
+    choice = input("Would you like to add load? (y/n): ")
+    if choice.lower() == "y":
+        add_load()
+    elif choice.lower() == "n":
+        print("Thank you for choosing us!")
+    else:
+        print("Please enter a valid choice.")
+        main()
+
+    pass
+
+while True:
+    main()
+    choice = input("\nDo you want to run the program again? (y/n): ")
+    if choice.lower() != "y":
+        break
